e83d1328d89c1fae7e8bdb7f825fa4d7c546ca86
Christian Bayquen <cgabayquen@gmail.com>
2024-03-12 22:11:14 +0800

    fix: sundays are now properly mapped in date-partition

--- src/lib/components/custom/date-partition.svelte
+++ src/lib/components/custom/date-partition.svelte
@@ -4,12 +4,15 @@
 
     let date = new Date(dateString);
 
+
     const monthMap: Record<number, string> = {
         0: "Jan", 1: "Feb", 2: "Mar", 3: "Apr", 4: "May", 5: "Jun", 6: "Jul", 7: "Aug", 8: "Sep", 9: "Oct", 10: "Nov", 11: "Dec"
     };
     const dayMap: Record<number, string> = { 
-        1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat", 7: "Sun"
+        1: "Mon", 2: "Tue", 3: "Wed", 4: "Thu", 5: "Fri", 6: "Sat", 0: "Sun"
     };
+
+    console.log(date.getDay(), dayMap[date.getDay()])
 
     let datePreview = dayMap[date.getDay()] + ", " + monthMap[date.getMonth()] + " " + date.getDate();
     const dateToday = new Date();
